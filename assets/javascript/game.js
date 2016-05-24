3bfac23407b4875da9579450b13249cf8b65ca9c
Dan Merkin <danmerkin@Dans-MBP.home>
2016-05-23 20:20:16 -0400

    updated2

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,37 @@
 alert('See if you can click the correct pictures to equal 76!')
 
 //Pictures from left to right
-var pic1 = 10
-var pic2 = 20
-var pic3 = 3
-var pic4 = 6
+var pic1 = 10;
+var pic2 = 20;
+var pic3 = 3;
+var pic4 = 6;
+
+var userGuesses = [];
+var userGuess = '';
+
+
+$(document).ready(function() {
+	$('.pic1').click(function() {
+		$('#userGuess').text(pic1);
+
+	});
+});
+
+$(document).ready(function() {
+	$('.pic2').click(function() {
+		$('#userGuess').text(pic2);
+
+	});
+});
+
+$(document).ready(function(){
+	$('.pic3').click(function(){
+		$('#userGuess').text(pic3)		
+	});
+});
+
+$(document).ready(function(){
+	$('.pic4').click(function(){
+		$('#userGuess').text(pic4)		
+	});
+});
